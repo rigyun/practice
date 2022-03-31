8329c2c3c3b5e32319e69bce44a75d0a1970f382
rigel.byun <rigel.byun@rigelbyuns-MacBook-Pro.local>
2022-03-31 21:09:26 +0900

    Montherapy 03312109

--- temp.py
+++ temp.py
@@ -2,9 +2,6 @@ import numpy as np
 
 aa = np.array([1,2,3])
 print(aa)
-<<<<<<< HEAD
-print("add by rigyun 1")
-=======
 
+print("add by rigyun 1")
 print("add by Montherapy:")
->>>>>>> e9e73e7 (Montherapy-03312053)
